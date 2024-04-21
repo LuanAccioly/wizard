1403b1287e9101c10293d75e84696741b810f7f1
Luan Accioly <acciolyluan@gmail.com>
2024-04-21 01:29:26 -0300

    corrigindo parede que prende

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Callbacks;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -14,11 +15,7 @@ public class Player : MonoBehaviour
   private bool isJumping;
   private bool doubleJump;
   private Animator anim;
-  private bool isWallSliding;
-  private float wallSlidingSpeed = 2f;
 
-  [SerializeField] private Transform wallCheck;
-  [SerializeField] private LayerMask wallLayer;
 
   void Start()
   {
@@ -32,32 +29,35 @@ public class Player : MonoBehaviour
     horizontal = Input.GetAxis("Horizontal");
     Move();
     Jump();
-    WallSlide();
   }
+
+
 
   void Move()
   {
-    Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-    transform.position += movement * Time.deltaTime * Speed;
+    // Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+    // transform.position += movement * Time.deltaTime * Speed;
 
-    if (Input.GetAxis("Horizontal") > 0f)
+    float movement = Input.GetAxis("Horizontal");
+    rig.velocity = new Vector2(movement * Speed, rig.velocity.y);
+    if (movement > 0f)
     {
       anim.SetBool("run", true);
     }
-    else if (Input.GetAxis("Horizontal") < 0f)
+    else if (movement < 0f)
     {
       anim.SetBool("run", true);
     }
-    else if (Input.GetAxis("Horizontal") == 0f)
+    else if (movement == 0f)
     {
       anim.SetBool("run", false);
     }
 
-    if (Input.GetAxis("Horizontal") > 0f)
+    if (movement > 0f)
     {
       transform.eulerAngles = new Vector3(0f, 0f, 0f);
     }
-    else if (Input.GetAxis("Horizontal") < 0f)
+    else if (movement < 0f)
     {
       transform.eulerAngles = new Vector3(0f, 180f, 0f);
     }
@@ -85,25 +85,6 @@ public class Player : MonoBehaviour
     }
   }
 
-  bool IsWalled()
-  {
-    return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
-  }
-
-  void WallSlide()
-  {
-    if (IsWalled() && horizontal != 0)
-    {
-      isWallSliding = true;
-      rig.velocity = new Vector2(rig.velocity.x, Mathf.Clamp(rig.velocity.y, -wallSlidingSpeed, float.MaxValue));
-    }
-    else
-    {
-      isWallSliding = false;
-    }
-  }
-
-
   void OnCollisionEnter2D(Collision2D collision)
   {
     if (collision.gameObject.layer == 8)
@@ -113,14 +94,8 @@ public class Player : MonoBehaviour
       anim.SetBool("doublejump", false);
     }
 
-
-    //  Se quiser que o player pare de pular quando estiver enroscado na parede, só descomentar o código abaixo	
-
-    if (collision.gameObject.layer == 7)
-    {
-      isJumping = true;
-    }
   }
+
   void OnCollisionExit2D(Collision2D collision)
   {
     if (collision.gameObject.layer == 8)
